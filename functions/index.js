bf1a4f4340c0cd142b243949982af1bfef0d365d
Mike DuRussel <heyitsmdr@gmail.com>
2022-11-09 10:08:49 -0500

    Increase clean messages job

--- functions/index.js
+++ functions/index.js
@@ -65,8 +65,24 @@ exports.broadcastMessage = functions.https.onCall(async (data, context) => {
   
   await admin.firestore().collection('networks').doc(user.data().network).collection('messages').add({
     nick: user.data().nick,
-    message: data.message
+    message: data.message,
+    created: new Date()
   });
 
   return { success: true };
+});
+
+exports.cleanOldMessages = functions.pubsub.schedule('every hour').onRun(async () => {
+  const col = admin.firestore().collection('networks');
+  const fiveMinsAgo = new Date(Date.now() - (5 * 60000));
+  const networks = await col.get();
+  networks.forEach(async network => {
+    const messages = await col.doc(network.id).collection('messages').where('created', '<', fiveMinsAgo).get();
+    messages.forEach(async msg => {
+      console.log(`Deleting message ${msg.id}`);
+      await col.doc(network.id).collection('messages').doc(msg.id).delete();
+    });
+  });
+
+  return null;
 });
